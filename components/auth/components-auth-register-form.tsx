e662bbe0308ef1981ae30f47ee8c877fb1dfa4bb
Phú Thịnh <trafficuserio2024@gmail.com>
2024-12-31 11:54:30 +0700

    Update components-auth-register-form.tsx

--- components/auth/components-auth-register-form.tsx
+++ components/auth/components-auth-register-form.tsx
@@ -27,7 +27,7 @@ const ComponentsAuthRegisterForm = () => {
     };
 
     const validatePassword = (password: string) => {
-        const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        const re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d\S]{8,}$/;
         return re.test(password);
     };
 
